3c6f8d2718c32a76124e35181de0237c1de681d5
Joana <joanapiovaroli@gmail.com>
2021-06-19 23:51:05 -0300

    Add faker library

--- files/write/person.py
+++ files/write/person.py
@@ -1,11 +1,13 @@
 from faker import Faker
 import csv
 
+# user faker to create 1000 random people data
 output = open('data.csv','w')
 fake = Faker()
 header = ['name','age','street','city','state','zip','lng','lat']
 mywriter = csv.writer(output)
 mywriter.writerow(header)
+
 for r in range(1000):
     mywriter.writerow([
         fake.name(),
@@ -19,6 +21,7 @@ for r in range(1000):
     ])
 output.close()
 
+# read the .csv file as a dictionary
 with open('data.csv') as f:
     myreader = csv.DictReader(f)
     headers = next(myreader)
