311b51d088566d92f205c8b736845f856a4c2d97
Joana <joanapiovaroli@gmail.com>
2021-06-19 23:50:56 -0300

    Uncomment file

--- files/write/person_json.py
+++ files/write/person_json.py
@@ -2,31 +2,33 @@ from faker import Faker
 import json
 import pandas.io.json as pd_JSON
 
-# output = open('data.json','w')
-# fake = Faker()
-# alldata = {}
-# alldata['records'] = []
+# user faker to create 1000 random people data
+output = open('data.json','w')
+fake = Faker()
+alldata = {}
+alldata['records'] = []
 
-# for x in range(1000):
-#     data = {
-#         "name":fake.name(),
-#         "age":fake.random_int(min=18, max=80, step=1),
-#         "street":fake.street_address(),
-#         "city":fake.city(),
-#         "state":fake.state(),
-#         "zip":fake.zipcode(),
-#         "lng":float(fake.longitude()),
-#         "lat":float(fake.latitude())
-#         }
-#     alldata['records'].append(data)
+for x in range(1000):
+    data = {
+        "name":fake.name(),
+        "age":fake.random_int(min=18, max=80, step=1),
+        "street":fake.street_address(),
+        "city":fake.city(),
+        "state":fake.state(),
+        "zip":fake.zipcode(),
+        "lng":float(fake.longitude()),
+        "lat":float(fake.latitude())
+        }
+    alldata['records'].append(data)
 
-# json.dump(alldata, output)
+json.dump(alldata, output)
 
-# with open("data.json","r") as f:
-#     data = json.load(f)
+# decode the .json file
+with open("data.json","r") as f:
+    data = json.load(f)
 
-# print(type(data))
-# print(data['records'][0]['name'])
+print(type(data))
+print(data['records'][0]['name'])
 
 f = open('data.json','r')
 data = pd_JSON.loads(f.read())
