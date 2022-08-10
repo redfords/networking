495f24ce7aa313c5308989f5d0f4ceb41f94ae21
redfords <joanapiovaroli@gmail.com>
2022-08-09 23:45:17 -0300

    Add get_month, parser

--- webscraping/cotizacion.py
+++ webscraping/cotizacion.py
@@ -25,4 +25,10 @@ def get_month(date, delta):
     y = date.year + ((date.month) + delta - 1) // 12
     if not m: m = 12
     d = min(date.day, [31,
-                       29 if y % 4])
+                       29 if y % 4 == 0 and (not y % 100 == 0 or y % 400 == 0) else 28,
+                       31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m - 1])
+    return date.replace(day=d, month=m, year=y)
+
+def parser(filas, fecha_desde):
+    tabla_final = pd.DataFrame()
+    
