89ffe5d5fd3ff523620f840d7a22fd7a8f5821f8
redfords <joanapiovaroli@gmail.com>
2022-08-15 03:37:55 -0300

    Replace variable names

--- webscraping/cotizacion_uva.py
+++ webscraping/cotizacion_uva.py
@@ -6,32 +6,32 @@ import pandas as pd
 import os
 import sys
 
-def scrap(fecha_desde, fecha_hasta):
+def scrap(from_date, to_date):
     url = 'http://www.bcra.gov.ar/PublicacionesEstadisticas/Principales_variables_datos.asp'
     try:
-        data = {'fecha_desde': fecha_desde, 'fecha_hasta': fecha_hasta, 'B1': 'Enviar', 'primeravez': 1, 'serie': 7913}
+        data = {'fecha_desde': from_date, 'fecha_hasta': to_date, 'B1': 'Enviar', 'primeravez': 1, 'serie': 7913}
         resp = requests.post(url=url, data=data)
         soup = BeautifulSoup(resp.text, "html.parser")
 
     except requests.exceptions.RequestException as e:
         raise SystemExit(e)
 
-    filas = soup.find_all('td')
-    return filas
+    rows = soup.find_all('td')
+    return rows
 
-def parsear(filas):
+def parsear(rows):
     mensual = pd.DataFrame()
-    for i in range(0, int(len(list(filas)) / 2)):
+    for i in range(0, int(len(list(rows)) / 2)):
         dic = dict()
-        dic['fecha'] = filas[2 * i].text.strip()
-        dic['valor'] = filas[2 * i + 1].text.strip()
+        dic['fecha'] = rows[2 * i].text.strip()
+        dic['valor'] = rows[2 * i + 1].text.strip()
 
         rueda = pd.DataFrame.from_dict(dic, orient='index').transpose().set_index('fecha')
         rueda.index = pd.to_datetime(rueda.index, format='%d/%m/%Y')
         mensual = pd.concat([mensual, rueda], axis=0)
     return mensual
 
-def monthdelta(date, delta):
+def get_month(date, delta):
     m, y = (date.month + delta) % 12, date.year + ((date.month) + delta - 1) // 12
     if not m: m = 12
     d = min(date.day, [31,
@@ -39,25 +39,26 @@ def monthdelta(date, delta):
                        31, 30, 31, 30, 31, 31, 30, 31, 30, 31][m - 1])
     return date.replace(day=d, month=m, year=y)
 
-def periodo_descarga(fecha_desde, fecha_hasta, path, year, month, day):
+def periodo_descarga(from_date, to_date, path, date):
     tabla_anual = pd.DataFrame()
-    filas = scrap(fecha_desde, fecha_hasta)
-    tabla = parsear(filas)
+    rows = scrap(from_date, to_date)
+    tabla = parsear(rows)
     tabla_anual = pd.concat([tabla_anual, tabla], axis=0)
 
-    fullPath = path + '/' + str(year) + '/' + str(month)
-    if not os.path.exists(fullPath):
-        os.makedirs(fullPath)
+    year = f'{date:%Y}'
+    month = f'{date:%m}'
+    day = f'{date:%d}'
 
-    archivo = 'bcra_cotizacion_uva_' + str(year) + str(month) + str(day) + '.txt.gz'
-    tabla_anual.to_csv(fullPath + '/' + archivo, sep='|')
+    full_path = path + '/' + year + '/' + month
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
 
-FechaDesde = monthdelta(datetime.datetime.now(), -6)
-FechaHasta = datetime.datetime.now()
+    file = 'bcra_cotizacion_uva_' + year + month + day + '.txt.gz'
+    tabla_anual.to_csv(full_path + '/' + file, sep='|')
+
+from_date = get_month(datetime.datetime.now(), -6)
+to_date = datetime.datetime.now()
 path = str(sys.argv[1])
-fecha = datetime.date.today()
-year = fecha.year
-month = '{:02d}'.format(fecha.month)
-day = '{:02d}'.format(fecha.day)
+date = datetime.date.today()
 
-periodo_descarga(FechaDesde.strftime('%Y-%m-%d'), FechaHasta.strftime('%Y-%m-%d'), path, year, month, day)
+periodo_descarga(from_date.strftime('%Y-%m-%d'), to_date.strftime('%Y-%m-%d'), path, date)
