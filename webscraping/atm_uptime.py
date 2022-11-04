6d982f4c57e52701313faf253ed03ff0d49349d5
Joana Piovaroli <joanapiovaroli@gmail.com>
2022-11-04 09:39:29 -0300

    Update atm_uptime.py

---
+++ webscraping/atm_uptime.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from time import sleep
+import pickle
+import re
+import os
+
+class Webscrapping():
+        return 0
+    
+WebScraping().execute()
